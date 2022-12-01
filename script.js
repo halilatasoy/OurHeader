f593e19b83327212f8b5db63d00df0b7cdf12e9d
Halil Atasoy <119619122+halilatasoy@users.noreply.github.com>
2022-12-01 20:44:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var headerstr = new String("Our Header");
+document.write("<h1>" + headerstr + "</h1>");
+
+var dateToday = new Date();
+document.write("Today is " + dateToday + " <br><br><br>");
+
+document.write(" <p>" + Math.PI + " ifadesi özel bir değere sahiptir. </p>");
+
+document.write(" <p style= 'margin-top:50px';>" +
+    Math.sqrt(9) + " sayısı 9'un kareköküdür. </p>");
+
+document.write(" <p style= 'margin-top:50px';>" +
+    Math.random() * 100 + " sayısı 0 ile 100 arasında rastgele seçtiğimiz bir sayıdır. Bu sayıyı fonksiyon seçmiştir. </p>");
